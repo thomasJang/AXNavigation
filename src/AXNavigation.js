afa75c33b937aebd6d6b7d64dba6c5feb454eb9f
ThomasJ <tom@axisj.com>
2015-11-18 17:11:16 +0900

    core update

--- src/AXNavigation.js
+++ src/AXNavigation.js
@@ -26,12 +26,25 @@ var AXNavigation = (function() {
 		return _target;
 	}
 
-	var e_click = ('ontouchstart' in window) ? "touchstart" : "click";
+	//var e_click = ('ontouchstart' in window) ? "touchstart" : "click";
+	var e_click = "click";
+	var e_touch_start, e_touch_move, e_touch_end;
+	if ('ontouchstart' in document.documentElement) {
+		e_touch_start = "touchstart";
+		e_touch_move = "touchmove";
+		e_touch_end = "touchend";
+	}
+	else {
+		e_touch_start = "mousedown";
+		e_touch_move = "mousemove";
+		e_touch_end = "mouseup";
+	}
 
 	function klass() {
 
 		(function() {
 			this.config = {
+				swipe: true,
 				animateTime: 300,
 				addon_back: {
 					html: '<i class="icon-dp-back"></i>'
@@ -71,6 +84,9 @@ var AXNavigation = (function() {
 			 */
 
 			this.device_width = $(document.body).width();
+
+			this.ready_swipe();
+
 			return this;
 		};
 
@@ -86,16 +102,16 @@ var AXNavigation = (function() {
 				}
 
 				if (target == "_right") {
-					po.push('<div class="bar-addon left" data-header-btn="back">' + cfg.addon_back.html);
+					po.push('<a class="bar-addon left" data-header-btn="back">' + cfg.addon_back.html);
 					if (cfg.toolbar_previous_title_show)
 						po.push('<span class="label">' + last_content.form.header.title + '</span>');
-					po.push('</div>');
+					po.push('</a>');
 				}
 
 				if (form.header.addon) {
 					for (var i = 0, l = form.header.addon.length, addon; i < l; i++) {
 						addon = form.header.addon[i];
-						po.push('<div class="bar-addon ' + addon.type + '" data-header-btn="' + i + '">' + addon.html + '</div>');
+						po.push('<a class="bar-addon ' + addon.type + '" data-header-btn="' + i + '">' + addon.html + '</a>');
 					}
 				}
 				po.push('<div style="clear: both;"></div>');
@@ -147,12 +163,15 @@ var AXNavigation = (function() {
 				header_jdom.addClass("slide-in-right");
 			}
 			else {
-				header_jdom.addClass("slide-in-" + option);
+				if (option == "fade") header_jdom.addClass("fade-in");
+				else header_jdom.addClass("slide-in-" + option);
+
 			}
 
 			if (last_content) {
 				last_content.header_jdom.attr("class", "bar-wrapper");
-				last_content.header_jdom.addClass("slide-out-" + (option || "left"));
+				if (option == "fade") last_content.header_jdom.addClass("fade-out");
+				else last_content.header_jdom.addClass("slide-out-" + (option || "left"));
 			}
 		};
 
@@ -229,7 +248,7 @@ var AXNavigation = (function() {
 		};
 
 		//
-		this.open = function(form, target, option) {
+		this.open = function(form, target, option, param) {
 			if (this.animating) return this;
 			if (this.aside_opend) return this;
 
@@ -258,16 +277,18 @@ var AXNavigation = (function() {
 				if (this.active_content_main != "" && this.content_tree[this.active_content_main].length > 0) {
 					last_content = this.content_tree[this.active_content_main][this.content_tree[this.active_content_main].length - 1];
 					last_content_out_effect = "";
-
+					
 					if (option == "fade") {
 						last_content_out_effect = "fade-out";
 					}
 					else {
 						last_content_out_effect = "slide-out-" + (option || "left");
 					}
+
 					last_content.jdom.attr("class", cfg.content_wrapper_class_name + (function() {
 							return (last_content.form.className) ? " " + last_content.form.className : "";
 						})());
+
 					last_content.jdom.addClass(last_content_out_effect);
 					if (last_content.header_jdom) {
 						last_content.header_jdom.addClass(last_content_out_effect);
@@ -282,9 +303,6 @@ var AXNavigation = (function() {
 					cfg.content_container.prepend(div);
 
 					jdom = cfg.content_container.find("#axn-content-" + form.objectName);
-					if (transitioning) {
-						jdom.addClass("fade-in");
-					}
 
 					content = {
 						form: form,
@@ -298,9 +316,13 @@ var AXNavigation = (function() {
 					}
 
 					$.get(form.url, function(data) {
+						jdom.addClass("fade-in");
 						jdom.append(data);
 						if (window[form.objectName]) {
-							window[form.objectName][form.startFunction]();
+							setTimeout(function() {
+								window[form.objectName][form.startFunction]();
+
+							}, 1);
 						}
 					});
 
@@ -330,8 +352,11 @@ var AXNavigation = (function() {
 						this.open_header(form, target, option, content, last_content);
 					}
 
-					if (window[form.objectName] && window[form.objectName].focus) {
-						window[form.objectName].focus();
+					if (window[form.objectName] && window[form.objectName].active) {
+						window[form.objectName].active();
+						setTimeout(function() {
+							$(window).resize();
+						}, 1);
 					}
 				}
 
@@ -339,37 +364,38 @@ var AXNavigation = (function() {
 			}
 			else if (target == "_right") {
 
-				div = '<div class="' + cfg.content_wrapper_class_name + ' ' + (form.className || "") + '" id="axn-content-' + form.objectName + '" data-content-target="' + target + '"></div>';
+				div = '<div class="' + cfg.content_wrapper_class_name + ' ' + (form.className || "") + '" id="axn-content-' + form.objectName + '" data-content-target="' + target + '" style="display:none;"></div>';
 				cfg.content_container.append(div);
 				jdom = cfg.content_container.find("#axn-content-" + form.objectName);
 
 				content = {
 					form: form,
-					jdom: jdom
+					jdom: jdom,
+					param: param
 				};
-
-				jdom.addClass("slide-in-right");
 
 				// 직전 컨텐츠 이펙트 처리
 				last_content = this.content_tree[this.active_content_main][this.content_tree[this.active_content_main].length - 1];
 				last_content.jdom.attr("class", cfg.content_wrapper_class_name + (function() {
 						return (last_content.form.className) ? " " + last_content.form.className : "";
 					})());
-				last_content.jdom.addClass("slide-out-left");
-				if (last_content.header_jdom) {
-					last_content.header_jdom.addClass("slide-out-left");
-				}
+
+				//last_content.jdom.addClass("slide-out-left");
 
 				// make tool_bar
 				if (form.header) {
 					// after push content_stack
-					this.open_header(form, target, false, content, last_content);
+					this.open_header(form, target, "fade", content, last_content);
 				}
 
 				$.get(form.url, function(data) {
+					jdom.show().addClass("slide-in-right");
 					jdom.append(data);
+
 					if (window[form.objectName]) {
-						window[form.objectName][form.startFunction]();
+						setTimeout(function() {
+							window[form.objectName][form.startFunction](param);
+						}, 1);
 					}
 				});
 			}
@@ -421,10 +447,10 @@ var AXNavigation = (function() {
 			this.check_tool_tab_bar(_form);
 
 			//this.content_stack[this.content_stack.length - 2].jdom.show().removeClass("slide-out-left").addClass("slide-in-left");
-			content_root[content_root.length - 2].jdom.removeClass("slide-out-left").addClass("slide-in-left");
+			content_root[content_root.length - 2].jdom.removeClass("slide-out-left");
 			content_root[content_root.length - 1].jdom.removeClass("slide-in-right").addClass("slide-out-right");
 			if (_form.header) {
-				content_root[content_root.length - 2].header_jdom.removeClass("slide-out-left").addClass("slide-in-left");
+				content_root[content_root.length - 2].header_jdom.removeClass("fade-out");
 				content_root[content_root.length - 1].header_jdom.removeClass("slide-in-right").addClass("slide-out-right");
 			}
 
@@ -435,13 +461,15 @@ var AXNavigation = (function() {
 					content_root[content_root.length - 2].header_jdom.removeClass("slide-in-left");
 					content_root[content_root.length - 1].header_jdom.remove();
 				}
-				content_root.pop();
 
-				if (window[_form.objectName]) {
-					delete window[_form.objectName];
+				if (window[content_root[content_root.length - 1].objectName]) {
+					delete window[content_root[content_root.length - 1].objectName];
 				}
+
+				content_root.pop();
 				this.animating = false;
 			}).bind(this), cfg.animateTime);
+
 		};
 
 		// 유지할 필요가 없는 컨텐츠를 정리
@@ -559,6 +587,7 @@ var AXNavigation = (function() {
 			// todo : 지우기 전에 초기화
 			var i = content_root.length - 1;
 			var form = content_root[i].form;
+			var param = content_root[i].param;
 			//this.content_stack[i].jdom.remove();
 			if (window[content_root[i].form.objectName]) {
 				delete window[content_root[i].form.objectName];
@@ -569,11 +598,151 @@ var AXNavigation = (function() {
 			$.get(form.url, function(data) {
 				jdom.append(data);
 				if (window[form.objectName]) {
-					window[form.objectName][form.startFunction]();
+					window[form.objectName][form.startFunction](param);
 				}
 			});
-		}
+		};
+		
+		this.get_mouse = function(e) {
+			var mouse = {};
+			if('originalEvent' in e){
+				e = e.originalEvent;
+			}
+			if ('changedTouches' in e) {
+				mouse.x = Number(e.changedTouches[0].pageX);
+				mouse.y = Number(e.changedTouches[0].pageY);
+			}
+			else if ('touches' in e && e.touches[0]) {
+				mouse.x = Number(e.touches[0].pageX);
+				mouse.y = Number(e.touches[0].pageY);
+			}
+			else {
+				mouse.x = Number(e.pageX);
+				mouse.y = Number(e.pageY);
+			}
+			return mouse;
+		};
+		
+		this.ready_swipe = function() {
+			if (cfg.swipe) {
+				var touch_start = {}, touch_end = {};
+				this.swipe_fn = {
+					touch_start: function(e) {
 
+						var _self = this;
+						var content_root = _this.content_tree[_this.active_content_main];
+						if(content_root.length < 2) return false;
+
+						touch_start = {
+							mouse: _this.get_mouse(e),
+							time: (new Date()).getTime(),
+							move_content: content_root[content_root.length - 1].jdom,
+							position: content_root[content_root.length - 1].jdom.position()
+						};
+
+						touch_start.move_content.removeClass("touch-end");
+						
+						//console.log(touch_start);
+
+						$(document.body).bind(e_touch_move + ".axnavigation", function(e) {
+							_self.touch_move(e || window.event);
+						});
+						$(document.body).bind(e_touch_end + ".axnavigation", function(e) {
+							_self.touch_end(e || window.event);
+						});
+
+						$(document.body).attr("onselectstart", "return false;");
+					},
+					touch_move: function(e) {
+
+						var mouse = _this.get_mouse(e);
+						if (!touch_start.direction) {
+							var dx = Math.abs(touch_start.mouse.x - mouse.x), dy = Math.abs(touch_start.mouse.y - mouse.y) * 2;
+							if(dx > 2) {
+								touch_start.direction = (dx > dy) ? "X" : "Y";
+							}
+						}
+
+						// 터치 방향 판단 하여 수평일 때만
+						if (touch_start.direction == "X") {
+							var new_left = Number(touch_start.position.left) - (touch_start.mouse.x - mouse.x);
+							if(new_left < 0) {
+
+								//todo : aside 가 있는 경우 별도 처리리
+								$(document.body).unbind(e_touch_move + ".axnavigation");
+								$(document.body).unbind(e_touch_end + ".axnavigation");
+								$(document.body).removeAttr("onselectstart");
+
+								if (e.preventDefault) e.preventDefault();
+								if (e.stopPropagation) e.stopPropagation();
+								e.cancelBubble = true;
+
+								return false;
+							}
+							touch_start.move_content.css({left: new_left});
+
+							touch_start.time = (new Date()).getTime();
+							touch_start.new_left = new_left;
+
+							if (e.preventDefault) e.preventDefault();
+							if (e.stopPropagation) e.stopPropagation();
+							e.cancelBubble = true;
+						}else{
+							$(document.body).unbind(e_touch_move + ".axnavigation");
+							$(document.body).unbind(e_touch_end + ".axnavigation");
+							$(document.body).removeAttr("onselectstart");
+						}
+					},
+					touch_end: function(e) {
+
+						touch_end = {
+							mouse: _this.get_mouse(e),
+							time: (new Date()).getTime()
+						};
+
+						var du_time = touch_end.time - touch_start.time, new_left = touch_start.new_left;
+
+						if(touch_start.direction == "X") {
+
+							if (du_time < 120 && Math.abs((touch_start.mouse.x - touch_end.mouse.x)) > 5) {
+								_this.close();
+							}
+							else if(new_left > 100){
+								_this.close();
+							}
+							else {
+								touch_start.move_content.css({left: 0}).addClass("touch-end");
+							}
+							if (e.preventDefault) e.preventDefault();
+							if (e.stopPropagation) e.stopPropagation();
+							e.cancelBubble = true;
+
+						}
+
+						$(document.body).unbind(e_touch_move + ".axnavigation");
+						$(document.body).unbind(e_touch_end + ".axnavigation");
+						$(document.body).removeAttr("onselectstart");
+
+
+						/*
+						 if (typeof new_left != "undefined") {
+						 $(document.body).unbind(e_touch_move + ".axnavigation");
+						 $(document.body).unbind(e_touch_end + ".axnavigation");
+						 $(document.body).removeAttr("onselectstart");
+
+						 if (e.preventDefault) e.preventDefault();
+						 if (e.stopPropagation) e.stopPropagation();
+						 e.cancelBubble = true;
+						 }
+						 */
+					}
+				};
+
+				cfg.content_container.on(e_touch_start, function(e) {
+					_this.swipe_fn.touch_start(e || window.event);
+				});
+			}
+		}
 	}
 
 	return klass;
